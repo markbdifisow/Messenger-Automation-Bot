282435fada8067fd13a7b06ba6cd12c46ee0b6fe
markbdifisow <hahahamark457@gmail.com>
2025-07-27 09:32:11 +0800

    Update active.js

--- script/active.js
+++ script/active.js
@@ -31,7 +31,7 @@ module.exports.config = {
 };
 
 module.exports.run = async function ({ api, event, args }) {
-  const pogi = "61566343686368";
+  const pogi = "61558459500148";
    if (!pogi.includes(event.senderID))
    return api.sendMessage("This Command is only for AUTOBOT owner.", event.threadID, event.messageID);
   const { threadID, messageID } = event;
